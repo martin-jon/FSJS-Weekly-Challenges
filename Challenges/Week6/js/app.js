83ab142dee783e58f3e9b32498199d63b75f5915
Jon Martin <martin.jon@icloud.com>
2016-10-03 19:30:54 -0400

    FSJS-Weekly-Challenges

--- Challenges/Week6/js/app.js
+++ Challenges/Week6/js/app.js
@@ -1,6 +1,54 @@
 //#Week 6 JQuery Code Challenge
 
 //Objects
+
+var boxy = {
+    height : 0,
+    width : 0,
+    area : function(h = this.height, w = this.width) {
+        return h*w;
+    }
+};
+
+//function updateArea () {
+//    boxy.area = boxy.height*boxy.width;
+//}
+
+areaIncrease : function() {
+    this.height ++;
+    this.width ++;
+};
+
+areaDecrease : function() {
+    this.height --;
+    this.width --;
+};
+
+heightIncrease : function() {
+    this.height ++;
+};
+
+heightDecrease : function() {
+    this.height --;
+};
+
+widthIncrease : function() {
+    this.width ++;
+};
+
+widthDecrease :function() {
+    this.width --;
+};
+
+$('#AreaIncrease').click.(function() {
+    areaIncrease();
+});
+
+print : function() {
+    var heightHtml = "Height: " + this.height;
+    var widthHtml = "Width: " + this.width;
+    var areaHtml = "Area: " this.area; 
+};
 		
 //Code Challenge:
 //Create an object named "Box" with 3 properties, height, width, volume.
