2c1f62c022a0a46e7a70cc39f1b9b4a0102b7a6e
Jon Martin <martin.jon@icloud.com>
2016-10-11 14:20:55 -0400

    FSJS Weekly Challenges

--- Challenges/Week7/app.js
+++ Challenges/Week7/app.js
@@ -1,1 +1,11 @@
 //See readme for challenge instructions
+
+var github = require('./github.js');
+console.log(process.argv);
+var usernames = process.argv.slice(2) || ["josephmudd","codelouisville", "bumbolio"];
+usernames.forEach(function(user) {
+    github.getRepos(user).then(function(repolist) {
+        
+        
+    })
+})
--- Challenges/Week7/github.js
+++ Challenges/Week7/github.js
@@ -1,5 +1,15 @@
 //See readme for challenge instructions
 
+return new Promise( function(resolve, reject) {
+    var options = {
+        host: 'api.github.com',
+        path: '/users/' + username + '/repos',
+        headers: {
+            'User-Agent': "bumbolio"
+        }
+    }
+})
+
 /*
  * Note about github api: requires User-Agent header to be set. This can be done
  * in Node by passing an options object (rather than a simple url string) as the 
